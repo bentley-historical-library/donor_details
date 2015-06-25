db212d956f0948892b09738c35f4d26db23b464a
djpillen <djpillen@umich.edu>
2015-06-25 12:19:58 -0400

    aesthetics

--- version_1.1.2/backend/model/donor_detail.rb
+++ version_1.1.2/backend/model/donor_detail.rb
@@ -7,16 +7,16 @@ class DonorDetail < Sequel::Model(:donor_detail)
 	set_model_scope :global
 
 	auto_generate 	:property => :number,
-				  	:generator => proc { |json|
-				  	donors = DonorDetail.select(:number).map(&:number)    
-    				donors_int = donors.map {|i| i.to_i}
-                    # note: this does not look for the name "max"
-    				max_donor = donors_int.max
-    				new_donor_int = max_donor + 1
-    				new_donor = new_donor_int.to_s
-    				new_donor
-				  },
-				  :only_if => proc { |json| json["number_auto_generate"]}
+			:generator => proc { |json|
+			donors = DonorDetail.select(:number).map(&:number)    
+    			donors_int = donors.map {|i| i.to_i}
+                        # note: this does not look for the name "max"
+    			max_donor = donors_int.max
+    			new_donor_int = max_donor + 1
+    			new_donor = new_donor_int.to_s
+    			new_donor
+			},
+			:only_if => proc { |json| json["number_auto_generate"]}
     
   def validate
     super
