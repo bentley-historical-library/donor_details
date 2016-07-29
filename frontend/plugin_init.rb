e431980df75b8e51c62e86a99288adff298b3b97
djpillen@umich.edu <djpillen@umich.edu>
2016-07-29 08:56:11 -0400

    little bit of formatting

--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -1,7 +1,6 @@
 Rails.application.config.after_initialize do
 	
 	SearchResultData.class_eval do
-	
 		def self.AGENT_FACETS
 			["primary_type", "source", "rules", "is_donor_u_sbool"]
 		end
--- indexer/donor_details_indexer.rb
+++ indexer/donor_details_indexer.rb
@@ -2,12 +2,12 @@ class CommonIndexer
   add_indexer_initialize_hook do |indexer|
     indexer.add_document_prepare_hook {|doc, record|
       if  ['agent_person', 'agent_family', 'agent_software', 'agent_corporate_entity'].include?(doc['primary_type']) && record['record']['donor_details']
-	doc['is_donor_u_sbool'] = !record['record']['donor_details'].collect{|donor| donor['donor_number']}.compact.empty?
+	      doc['is_donor_u_sbool'] = !record['record']['donor_details'].collect{|donor| donor['donor_number']}.compact.empty?
         doc['donor_number_u_sstr'] = record['record']['donor_details'].collect{|donor| donor['donor_number']}.compact
         doc['dart_id_u_sstr'] = record['record']['donor_details'].collect{|donor| donor['dart_id']}.compact
         doc['beal_contact_id_u_sstr'] = record['record']['donor_details'].collect{|donor| donor['beal_contact_id']}.compact
         doc['fullrecord'] << "#{record['record']['donor_details'].collect{|donor| donor['donor_number']}.compact}"
-	end
+	    end
     }
   end
 end
