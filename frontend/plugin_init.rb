35e28b475a91fb0f7c66224aa0c4bdab2c2aac57
djpillen@umich.edu <djpillen@umich.edu>
2016-07-29 16:35:19 -0400

    little more indentation cleanup

--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -1,11 +1,11 @@
 Rails.application.config.after_initialize do
-	
-	SearchResultData.class_eval do
-		def self.AGENT_FACETS
-			["primary_type", "source", "rules", "is_donor_u_sbool"]
-		end
-	end
-	        
-	AspaceFormHelper::PROPERTIES_TO_EXCLUDE_FROM_READ_ONLY_VIEW << "donor_number_auto_generate"
+    
+    SearchResultData.class_eval do
+        def self.AGENT_FACETS
+            ["primary_type", "source", "rules", "is_donor_u_sbool"]
+        end
+    end
+            
+    AspaceFormHelper::PROPERTIES_TO_EXCLUDE_FROM_READ_ONLY_VIEW << "donor_number_auto_generate"
 
 end
