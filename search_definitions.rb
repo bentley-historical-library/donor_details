f2af4c9657faadc0ba5dabf798df1a7abca60a49
djpillen@umich.edu <djpillen@umich.edu>
2017-06-14 09:28:57 -0400

    add DART LID to advanced search

--- search_definitions.rb
+++ search_definitions.rb
@@ -1,1 +1,2 @@
 AdvancedSearch.define_field(:name => 'donor_number', :type => :text, :visibility => [:staff], :solr_field => 'donor_number_u_sstr')
+AdvancedSearch.define_field(:name => 'dart_id', :type => :text, :visibility => [:staff], :solr_field => 'dart_id_u_sstr')
