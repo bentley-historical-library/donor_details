9bae3702d2af8348f4aa0d49ba8c67a29b1843fe
djpillen@umich.edu <djpillen@umich.edu>
2017-10-13 12:48:18 -0400

    uncommited change

--- backend/model/donor_detail.rb
+++ backend/model/donor_detail.rb
@@ -9,19 +9,19 @@ class DonorDetail < Sequel::Model(:donor_detail)
     auto_generate   :property => :donor_number,
             :generator => proc { |json|
             donors = DonorDetail.select(:donor_number).map(&:donor_number)
-                donors_int = donors.map {|i| i.to_i}
-                new_donor_int = donors_int.max + 1
-                new_donor_int.to_s
+            donors_int = donors.map {|i| i.to_i}
+            new_donor_int = donors_int.max + 1
+            new_donor_int.to_s
             },
-            :only_if => proc { |json| json["donor_number_auto_generate"]}
+            :only_if => proc { |json| json["donor_number_auto_generate"] && json["donor_number"].empty?}
 
     auto_generate   :property => :beal_contact_id,
             :generator => proc { |json|
             contacts = DonorDetail.select(:beal_contact_id).map(&:beal_contact_id)
-                contacts_int = contacts.map {|i| i.to_i}
-                new_contact_int = contacts_int.max + 1
-                new_contact_int.to_s
-            }
-
+            contacts_int = contacts.map {|i| i.to_i}
+            new_contact_int = contacts_int.max + 1
+            new_contact_int.to_s
+            },
+            :only_on_create => true
 
 end
