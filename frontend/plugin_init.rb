456753e1f555d7acc680836238f3f96f074588cf
Payten Giles <payten.giles@gmail.com>
2023-05-02 14:02:24 +1000

    Replace index/listing template overrides with some plugin_init magic to include the Donor Number column in result listings whenever an agent is present.  And also drop the authority id column on the agents browse listing.

--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -8,4 +8,28 @@ Rails.application.config.after_initialize do
             
     AspaceFormHelper::PROPERTIES_TO_EXCLUDE_FROM_READ_ONLY_VIEW << "donor_number_auto_generate"
 
+    ApplicationHelper.class_eval do
+        alias_method :render_aspace_partial_pre_donor_details, :render_aspace_partial
+        def render_aspace_partial(args)
+            if args[:partial] == "search/listing"
+                if controller.controller_name == 'agents' || ASUtils.wrap(@search_data['results']).any?{|result| ASUtils.wrap(result['types']).include?("agent")}
+                    action_column = @columns.pop if @columns.last.class.include?('actions')
+
+                    add_column(I18n.t("donor_detail.donor_number"),
+                               { :sortable => false },
+                               proc {|result|
+                                   result['donor_number_u_sstr'].join(', ') if result['donor_number_u_sstr']
+                               })
+
+                    @columns << action_column if action_column
+                end
+
+                if controller.controller_name == 'agents'
+                    @columns.reject!{|col| col.sort_by == 'authority_id'}
+                end
+            end
+
+            render_aspace_partial_pre_donor_details(args)
+        end
+    end
 end
