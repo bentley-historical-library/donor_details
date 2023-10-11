5e95e194b48dc785d68cf0952528441ef441c58d
Samuel Sciolla <ssciolla@umich.edu>
2023-10-11 12:14:03 -0400

    Use add_columns?

--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -13,6 +13,8 @@ Rails.application.config.after_initialize do
         def render_aspace_partial(args)
             if args[:partial] == "search/listing"
                 if controller.controller_name == 'agents' || ASUtils.wrap(@search_data['results']).any?{|result| ASUtils.wrap(result['types']).include?("agent")}
+                    add_columns
+
                     action_column = @columns.pop if @columns.last.class.include?('actions')
 
                     add_column(I18n.t("donor_detail.donor_number"),
